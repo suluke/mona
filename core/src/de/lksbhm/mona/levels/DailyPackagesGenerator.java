d622c5b6cd78f5d2c64df4bc90c9268785da5613
Lukas Böhm <suluke93@gmail.com>
2014-09-21 12:21:35 +0200

    Improve the way daily levels are being created

--- core/src/de/lksbhm/mona/levels/DailyPackagesGenerator.java
+++ core/src/de/lksbhm/mona/levels/DailyPackagesGenerator.java
@@ -1,6 +1,7 @@
 package de.lksbhm.mona.levels;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -14,7 +15,7 @@ import de.lksbhm.mona.Mona;
 class DailyPackagesGenerator {
 	private static final Random random = new RandomXS128();
 
-	public static int getNumberOfPackages(Calendar date) {
+	private static int getNumberOfPackages(Calendar date) {
 		int minPackages = LksBhmGame.getGame(Mona.class).getSettings().statics
 				.getMinimumNumberOfDailyPackages();
 		int maxPackages = LksBhmGame.getGame(Mona.class).getSettings().statics
@@ -23,28 +24,59 @@ class DailyPackagesGenerator {
 		return random.nextInt(maxPackages - minPackages) + minPackages;
 	}
 
-	public static Difficulty getDifficultyForPackage(int index, Calendar date) {
-		random.setSeed(date.getTimeInMillis() + index);
-		return Difficulty.fromNumber(random.nextInt(5));
+	private static void fillWithDifficulty(Difficulty d,
+			Difficulty[] difficulties) {
+		for (int i = 0; i < difficulties.length; i++) {
+			difficulties[i] = d;
+		}
+	}
+
+	private static Difficulty[] getDifficulties(Calendar date) {
+		Difficulty[] difficulties = new Difficulty[getNumberOfPackages(date)];
+		switch (date.get(Calendar.DAY_OF_WEEK)) {
+		case Calendar.MONDAY:
+			fillWithDifficulty(Difficulty.VERY_EASY, difficulties);
+			break;
+		case Calendar.TUESDAY:
+			fillWithDifficulty(Difficulty.EASY, difficulties);
+			break;
+		case Calendar.WEDNESDAY:
+			fillWithDifficulty(Difficulty.MEDIUM, difficulties);
+			break;
+		case Calendar.THURSDAY:
+			fillWithDifficulty(Difficulty.HARD, difficulties);
+			break;
+		case Calendar.FRIDAY:
+			fillWithDifficulty(Difficulty.VERY_HARD, difficulties);
+			break;
+		default:
+			random.setSeed(date.getTimeInMillis());
+			for (int i = 0; i < difficulties.length; i++) {
+				difficulties[i] = Difficulty.fromNumber(random.nextInt(5));
+			}
+			Arrays.sort(difficulties);
+			break;
+		}
+		return difficulties;
 	}
 
 	public static LevelPackageCollection getDailyPackages(Calendar date) {
-		int numberOfPacks = getNumberOfPackages(date);
+		Difficulty[] difficulties = getDifficulties(date);
 		LevelPackageCollection collection = new LevelPackageCollection(
-				numberOfPacks);
+				difficulties.length);
 		long seed;
 		long millis = date.getTimeInMillis();
 		Date d = new Date(millis);
 		String dayName;
 		LevelPackage newPack;
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
-		for (int i = 0; i < numberOfPacks; i++) {
+		for (int i = 0; i < difficulties.length; i++) {
 			random.setSeed(millis + i);
 			seed = random.nextLong();
 			dayName = date.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
 					Locale.getDefault());
 			newPack = new DailyPackage(formatter.format(d) + "-" + i,
-					getDifficultyForPackage(i, date), collection, seed, random);
+					difficulties[i], collection, seed, random);
 			newPack.setDisplayName(dayName + Integer.toString(i + 1));
 			collection.setPackage(i, newPack);
 		}
